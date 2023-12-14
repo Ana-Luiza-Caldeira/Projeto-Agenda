a852a465e702625d21cf70bbe7dadb4c82e5162c
AnaLuizaCaldeira <ana.caldeira@lymtech.com.br>
2023-12-14 10:13:29 -0300

    add requirements and change the database

--- agenda/project/settings.py
+++ agenda/project/settings.py
@@ -52,8 +52,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'teste',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
